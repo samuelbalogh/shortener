92adbd393fd28a1abcfcba1754b9ba4bd00e1195
Samu B <samubalogh@gmail.com>
2021-03-16 13:24:50 +0000

    store only the coutner; decode short url to counter when getting it

--- app.py
+++ app.py
@@ -14,34 +14,47 @@ STORE = {}
 # this could be a counter in Redis, which can be atomically incremented
 COUNTER = 0
 
-ABC = [char for char in ascii_letters + digits]
+ABC = [char for char in digits + ascii_letters]
+BASE = len(ABC)
 
 def encode(number):
+    """
+    Encode number in base62
+    """
     string = ''
     while(number > 0):
-        string = ABC[number % len(ABC)] + string
-        number //= len(ABC)
+        string = ABC[number % BASE] + string
+        number //= BASE
     return string
+
+
+def decode(string):
+    """
+    Decode base62 number to base 10 number
+    """
+    number = 0
+    for index, char in enumerate(string[::-1]):
+        number += ABC.index(char) * BASE ** index
+    return number
 
 
 @app.route('/shorten', methods=['POST'])
 def create():
     global COUNTER
-
     # TODO escape JS in url
     url = request.json.get('url')
 
     COUNTER += 1
-
     encoded = encode(COUNTER)
-    STORE[encoded] = url
+    STORE[COUNTER] = url
 
     return ROOT_URL + encoded
 
 
 @app.route('/<shortcode>')
 def get(shortcode):
-    long_url = STORE.get(shortcode)
+    decoded = decode(shortcode)
+    long_url = STORE.get(decoded)
     return redirect(long_url)
 
 
